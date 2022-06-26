b485e4bd086b17e5403292a78e484f8c387dee4c
viet.dinhduc <ducviet2504@gmail.com>
2022-06-26 19:59:09 +0700

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'  
+import { decrement, increment } from './redux/slices/counterSlice';
 
 function App() {
+  const dispatch = useDispatch()
+  const count = useSelector((state) => state.counter.value)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Viet dep trai vo dịch nhat vu tru</div>
+        <div className='btn-action'>
+            <button onClick={()=> dispatch(increment())}>Increase</button>
+            <button onClick={()=> dispatch(decrement())}>Decrease</button>
+          </div>
+          <div>Count : {count}</div>
       </header>
     </div>
   );
